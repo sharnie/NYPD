af952e3f9de60ad8cce742a23d43c0365a322fe3
Sharnie Ivery <sharnieivery@gmail.com>
2014-11-20 17:26:07 -0500

    exposed audio url in popup

--- popup.js
+++ popup.js
@@ -12,7 +12,8 @@ var UI = {
                   '<div id="album-title" class="music-label">'+ last.album +'</div>',
                 '</div>',
             '</section>',
-            '<a href="'+ last.url +'" id="download-btn" class="btn-primary font-bold text-center block btn-download" download="'+ (last.song + ' - ' + last.artist + ' - ' + last.album + '.mp4') +'">Download</a>'
+            '<a href="'+ last.url +'" id="download-btn" class="btn-primary font-bold text-center block btn-download" download="'+ (last.song + ' - ' + last.artist + ' - ' + last.album + '.mp4') +'">Download</a>',
+            '<input type="text" value="'+ last.url +'" class="audio-url" />'
         ];
         template = template.join("\n");
         document.getElementById('content').innerHTML = template;
